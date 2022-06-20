6b8231f6c9150e15ed5b01291a54ab2b67ba2441
Mahib7009 <mahib.7009@gmail.com>
2022-06-20 15:42:34 -0400

    initial addition

---
+++ proj2/proj2.js
@@ -0,0 +1,57 @@
+document.write(`
+    <div class="container px-4 py-5" id="hanging-icons">
+        <h2 class="pb-2 border-bottom">Arduino Voltmeter with LCD Display</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#toggles2"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Featured title</h2>
+                    <p>
+                        Paragraph of text beneath the heading to explain the
+                        heading. We'll add onto it with another sentence and
+                        probably just keep going until we run out of words.
+                    </p>
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#cpu-fill"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Featured title</h2>
+                    <p>
+                        Paragraph of text beneath the heading to explain the
+                        heading. We'll add onto it with another sentence and
+                        probably just keep going until we run out of words.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                        View Project On Github
+                    </a>
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#tools"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Featured title</h2>
+                    <p>
+                        Paragraph of text beneath the heading to explain the
+                        heading. We'll add onto it with another sentence and
+                        probably just keep going until we run out of words.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                        View code
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>`);
