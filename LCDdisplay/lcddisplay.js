d8b151765a3b4180b03bd4cba2beb4cf283d26e0
Mahib7009 <mahib.7009@gmail.com>
2022-07-15 20:23:39 -0400

    adding LCD Display proj

---
+++ LCDdisplay/lcddisplay.js
@@ -0,0 +1,63 @@
+document.write(`
+    <div class="container px-4 py-5" id="hanging-icons">
+        <h2 class="pb-2 border-bottom">LCD Monitor with with old parts</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#toggles2"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>About</h2>
+                    <p>
+                        I had an old DELL Inspiron All-in-One computer
+                        that wasn't being used. I did some research and 
+                        figured that I could use the LCD screen to make 
+                        a monitor/display.  
+                        
+                    </p>
+                    <img src="./LCDdisplay/pic1.jpg" alt="Fisrst pic of project" width="250">
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#cpu-fill"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Procsee</h2>
+                    <p>
+                        I took apart the All-in-one and isolated the LCD screen.
+                        I then got an LCD controller and power supply that were 
+                        compatible with the screen. I then assembled the display 
+                        and kept the outer chassy of the All in one to make the 
+                        monitor look more pleasing. (Bonus: there was a 4gb ram 
+                        stick in the all-in-one which I pluged into my Mac Mini
+                        to increase it's ram)
+                        
+                    </p>
+                    <img src="./LCDdisplay/pic2.jpg" alt="second pic of project" width="250">
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#tools"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Why</h2>
+                    <p>
+                        I did some Research into Monitor prices and saw 
+                        that even used monitors this size were going $80+. 
+                        This project cost a total of $37.95. This was the 
+                        better economical decision and I also learned a lot.
+                        
+                    </p>
+                    <img src="./LCDdisplay/pic3.jpg" alt="temporary setup" width="300">
+                </div>
+            </div>
+        </div>
+    </div>`);
