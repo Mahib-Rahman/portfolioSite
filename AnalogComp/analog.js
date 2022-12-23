51f62c08a9d266c69db3810210b292c451c9f8f7
Mahib Rahman <mahib.7009@gmail.com>
2022-12-22 21:58:52 -0500

    added analogComp

---
+++ AnalogComp/analog.js
@@ -0,0 +1,65 @@
+document.write(`
+    <div class="container px-4 py-5" id="hanging-icons">
+        <h2 class="pb-2 border-bottom">
+            Second Order Differrential Equation Analog Computer
+        </h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#toggles2"></use>
+                    </svg>
+                </div>   
+                <div>
+                    <h2>About</h2>
+                    <p>
+                        Built a secoond order differential equation solver.
+                        Below are the schematic and the physical circuit:  
+                        
+                    </p>
+                    <img src="./AnalogComp/pic1.jpg" alt="Fisrst pic of project" width="300">
+                    <img src="./AnalogComp/pic2.jpg" alt="Fisrst pic of project" width="250">
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#cpu-fill"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Procsee</h2>
+                    <p>
+                        Used an oscilloscope to measure the <b>Output Voltage</b>
+                        which is a solution to the second order differential 
+                        equation given the input voltage. This was possible thanks
+                        to capicitors. The way Capicitors accumulate charge and
+                        develop a voltage can be moveled with a differential equation.
+                        Thus, using two capicitors, we can model a second order differential 
+                        equation. We can measure the voltage readings at the output 
+                        which is the solution to the equation. By choosing values of the elements
+                        We can model different responces. The picture below shows the 
+                        oscilloscope measuring the "overdamped" responce.
+                    </p>
+                    <img src="./AnalogComp/pic3.jpg" alt="second pic of project" width="300">
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#tools"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Why</h2>
+                    <p>
+                    Given any second order differential equation, this circuit is able to model
+                    its solution using voltage.The picture below shows the Ltspice simulation 
+                    and an oscilloscope measuring the "underrdamped" responce of the circuit.
+                    </p>
+                    <img src="./AnalogComp/pic4.jpg" alt="temporary setup" width="300">
+                    <img src="./AnalogComp/pic5.jpg" alt="temporary setup" width="300">
+                </div>
+            </div>
+        </div>
+    </div>`);
